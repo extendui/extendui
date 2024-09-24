638d1aeef6e2496f8576587e47d43c6170954ce2
rynkovski <118986014+rynkovski@users.noreply.github.com>
2024-09-24 23:22:14 +0200

    feat: add social links

--- src/app/_components/Navbar/navbar.tsx
+++ src/app/_components/Navbar/navbar.tsx
@@ -1,13 +1,15 @@
-"use client";
+'use client';
 
-import Link from "next/link";
-import React, { ReactNode, useEffect, useRef, useState } from "react";
-import { ModeToggle } from "../ModeToggle/mode-toggle";
+import Link from 'next/link';
+import React, { ReactNode, useEffect, useRef, useState } from 'react';
+import { ModeToggle } from '../ModeToggle/mode-toggle';
+import { CodeXml } from 'lucide-react';
+import { Button } from '@/components/ui/button';
 
 type Props = {
   logo: ReactNode;
   navLinks: { name: string; href: string }[];
-  socialLinks: { name: string; href: string }[];
+  socialLinks: { name: string; href: string; icon?: ReactNode }[];
 };
 
 export const Navbar = ({ logo, navLinks, socialLinks }: Props) => {
@@ -27,17 +29,19 @@ export const Navbar = ({ logo, navLinks, socialLinks }: Props) => {
   return (
     <header ref={ref}>
       <div
-        className={`bg-gradient dark:bg-gradient-dark fixed inset-x-0 top-0 z-50 h-16 bg-repeat backdrop-blur duration-200 ${isIntersecting
-            ? "border-transparent bg-zinc-900/0"
-            : "bg-zinc-900/500 border-zinc-800"
-          }`}
+        className={`bg-gradient dark:bg-gradient-dark fixed inset-x-0 top-0 z-50 h-16 bg-repeat backdrop-blur duration-200 ${
+          isIntersecting
+            ? 'border-transparent bg-zinc-900/0'
+            : 'bg-zinc-900/500 border-zinc-800'
+        }`}
       >
         <div className="container mx-auto flex items-center justify-between p-5">
           <div className="flex justify-between gap-8">
             <Link
               href="/"
-              className="text-zinc-600 duration-200 hover:text-zinc-900 dark:text-zinc-200"
+              className="flex items-center justify-center gap-2 text-zinc-600 duration-200 hover:text-zinc-900 dark:text-zinc-200"
             >
+              <CodeXml />
               <div className="mr-4">{logo}</div>
             </Link>
             {navLinks.map((link) => (
@@ -52,12 +56,10 @@ export const Navbar = ({ logo, navLinks, socialLinks }: Props) => {
           </div>
           <div className="flex items-center gap-4">
             {socialLinks.map((link) => (
-              <Link
-                key={link.name}
-                href={link.href}
-                className="text-zinc-600 duration-200 hover:text-zinc-900 dark:text-zinc-200 dark:hover:text-zinc-300"
-              >
-                {link.name}
+              <Link key={link.name} href={link.href}>
+                <Button variant="outline" size="icon">
+                  {link.icon}
+                </Button>
               </Link>
             ))}
             <ModeToggle />
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,16 @@
-import { ThemeProvider } from "@/providers/themeProvider";
-import "@/styles/globals.css";
-import { GeistSans } from "geist/font/sans";
-import { type Metadata } from "next";
-import { Navbar } from "./_components/Navbar/navbar";
-import Layout from "./_components/Layout/layout";
-import Sidebar from "./_components/Sidebar/sidebar";
+import { ThemeProvider } from '@/providers/themeProvider';
+import '@/styles/globals.css';
+import { GeistSans } from 'geist/font/sans';
+import { type Metadata } from 'next';
+import { Navbar } from './_components/Navbar/navbar';
+import Layout from './_components/Layout/layout';
+import Sidebar from './_components/Sidebar/sidebar';
+import { Github, Twitter } from 'lucide-react';
 
 export const metadata: Metadata = {
-  title: "Extend UI",
-  description: "Extend UI is design website",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
+  title: 'Extend UI',
+  description: 'Extend UI is design website',
+  icons: [{ rel: 'icon', url: '/favicon.ico' }],
 };
 
 export default function RootLayout({
@@ -23,17 +24,23 @@ export default function RootLayout({
             <Navbar
               logo="Extend UI"
               navLinks={[
-                { name: "Components", href: "/components/button" },
-                { name: "Themes", href: "/themes" },
+                { name: 'Components', href: '/components' },
+                { name: 'Templates', href: '/templates' },
               ]}
               socialLinks={[
-                { name: "GitHub", href: "https://github.com/extendui/extendui" },
+                {
+                  name: 'GitHub',
+                  href: 'https://github.com/extendui/extendui',
+                  icon: <Github className="h-[1.2rem] w-[1.2rem]" />,
+                },
+                {
+                  name: 'Twitter',
+                  href: 'https://x.com/extendui_pro',
+                  icon: <Twitter className="h-[1.2rem] w-[1.2rem]" />,
+                },
               ]}
             />
-            <Layout
-              sidebar={<Sidebar items={[]} />}
-              children={children}
-            />
+            <Layout sidebar={<Sidebar items={[]} />} children={children} />
           </div>
         </ThemeProvider>
       </body>
