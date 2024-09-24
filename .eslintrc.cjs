fdad5992060c5e57aad4ee25102c6afd1bb4c761
Wicki Krzysztof <krzysiekwicki1@gmail.com>
2024-09-24 21:20:39 +0200

    preparation of project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,42 +1,87 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "project": true
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true, // Make sure tsconfig.eslint.json exists if you're using project-based linting
+    tsconfigRootDir: __dirname, // Specify the correct root directory for the project
   },
-  "plugins": [
-    "@typescript-eslint"
-  ],
-  "extends": [
+  plugins: ["@typescript-eslint", "import", "prettier"],
+  extends: [
     "next/core-web-vitals",
     "plugin:@typescript-eslint/recommended-type-checked",
-    "plugin:@typescript-eslint/stylistic-type-checked"
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:prettier/recommended", // Integrate Prettier with ESLint
   ],
-  "rules": {
+  rules: {
+    // TypeScript-specific rules
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
-        "prefer": "type-imports",
-        "fixStyle": "inline-type-imports"
-      }
+        prefer: "type-imports",
+        fixStyle: "inline-type-imports",
+      },
     ],
     "@typescript-eslint/no-unused-vars": [
       "warn",
       {
-        "argsIgnorePattern": "^_"
-      }
+        argsIgnorePattern: "^_", // Ignore unused variables prefixed with '_'
+      },
     ],
     "@typescript-eslint/require-await": "off",
     "@typescript-eslint/no-misused-promises": [
       "error",
       {
-        "checksVoidReturn": {
-          "attributes": false
-        }
-      }
-    ]
-  }
-}
+        checksVoidReturn: {
+          attributes: false,
+        },
+      },
+    ],
+
+    // Import sorting and organizing
+    "import/order": [
+      "error",
+      {
+        groups: [
+          ["builtin", "external"],
+          "internal",
+          ["parent", "sibling", "index"],
+        ],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+
+    // Prettier integration
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto", // Handle different OS line endings
+      },
+    ],
+
+    // React and Next.js specific rules
+    "react/react-in-jsx-scope": "off", // Next.js doesn't require React to be in scope
+    "jsx-a11y/anchor-is-valid": "off", // Next.js uses custom Link components
+  },
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      rules: {
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": "warn",
+      },
+    },
+  ],
+  settings: {
+    react: {
+      version: "detect", // Automatically detect the react version
+    },
+  },
+};
+
 module.exports = config;
--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,12 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
   plugins: ["prettier-plugin-tailwindcss"],
+  semi: true, // Add semicolons at the end of statements
+  singleQuote: true, // Use single quotes instead of double quotes
+  trailingComma: "all", // Add trailing commas where valid in ES5 (objects, arrays, etc.)
+  printWidth: 80, // Wrap lines that exceed 80 characters
+  tabWidth: 2, // Set the tab width to 2 spaces
+  endOfLine: "auto", // Handle different OS line endings
 };
 
 export default config;
