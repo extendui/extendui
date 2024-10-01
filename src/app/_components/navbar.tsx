05255f91b53e2fb53c07682595f6b230a4e79cf8
kicky1 <krzysiekwicki1@gmail.com>
2024-10-01 14:39:58 +0200

    Feat: add introduction section

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -59,7 +59,7 @@ export const Navbar = ({ navLinks, socialLinks }: Props) => {
               ))}
             </div>
           </div>
-          <div className="flex items-center gap-4">
+          <div className="flex items-center gap-2">
             <CommandMenu />
             {socialLinks.map((link) => (
               <Link key={link.name} href={link.href}>
