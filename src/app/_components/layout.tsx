67d559430c8bd27de4ea2a97c403f8257a38fdd4
rynkovski <118986014+rynkovski@users.noreply.github.com>
2024-10-03 18:38:13 +0200

    feat: update layout

--- src/app/_components/layout.tsx
+++ src/app/_components/layout.tsx
@@ -5,8 +5,8 @@ type Props = {
 
 export default function Layout({ sidebar, children }: Props) {
   return (
-    <div className="max-w-11xl mx-auto flex items-center justify-between p-5 pt-24 sm:px-6 lg:px-8">
-      <aside className="sticky top-24 mb-8 hidden h-[calc(100vh-5rem)] self-start overflow-y-auto pr-12 xl:block xl:w-1/5">
+    <div className="mx-auto flex max-w-11xl items-center justify-between p-5 pt-24 sm:px-6 lg:px-8">
+      <aside className="sticky top-24 mb-8 hidden h-[calc(100vh-5rem)] self-start overflow-y-auto pr-12 md:block xl:w-1/5">
         {sidebar}
       </aside>
       <main className="mb-4 flex-1">{children}</main>
