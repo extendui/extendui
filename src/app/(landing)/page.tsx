40f6375b3583627dafc74eac63d806b91338e894
rynkovski <118986014+rynkovski@users.noreply.github.com>
2024-10-16 14:54:56 +0200

    feat: add fadeup component

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -6,6 +6,7 @@ import Step from '@/components/step';
 import dynamic from 'next/dynamic';
 import ButtonExample from '@/showcase/Button/button';
 import ButtonSettingsEngine from '@/showcase/Button/settings-engine';
+import FadeUp from '@/components/fadeup';
 
 const DynamicSteps = dynamic(() => import('@/components/steps'));
 
@@ -16,44 +17,50 @@ export default function HomePage() {
         <div className="min-h-[calc(100dvh-4rem)] md:h-[100dvh] md:space-y-24">
           <div className="mx-auto flex max-w-7xl flex-col items-center space-y-4 py-[32dvh] text-center">
             <div className="relative">
-              <h1 className="bg-gradient-to-br from-black via-zinc-600 to-zinc-400 bg-clip-text text-center text-3xl font-bold tracking-tight dark:from-white dark:via-neutral-200 dark:to-black/[0.6] sm:text-center sm:text-4xl md:text-6xl">
-                Welcome to Extend UI
-              </h1>
-              <p className="mx-2 mt-6 max-w-2xl text-base font-light tracking-tight dark:text-zinc-300 sm:text-xl">
-                Reusable components built on{' '}
-                <span className="inline font-semibold">shadcn/ui</span> to
-                increase performance when building web applications.
-              </p>
-              <div className="mt-6 flex items-center justify-center gap-3">
-                <Link href="/docs/" passHref>
-                  <Button
-                    variant="default"
-                    className="flex items-center justify-center gap-1"
+              <FadeUp delay={0.2} duration={0.8}>
+                <h1 className="bg-gradient-to-br from-black via-zinc-600 to-zinc-400 bg-clip-text text-center text-3xl font-bold tracking-tight dark:from-white dark:via-neutral-200 dark:to-black/[0.6] sm:text-center sm:text-4xl md:text-6xl">
+                  Welcome to Extend UI
+                </h1>
+              </FadeUp>
+              <FadeUp delay={0.4} duration={0.8}>
+                <p className="mx-2 mt-6 max-w-2xl text-base font-light tracking-tight dark:text-zinc-300 sm:text-xl">
+                  Reusable components built on{' '}
+                  <span className="inline font-semibold">shadcn/ui</span> to
+                  increase performance when building web applications.
+                </p>
+              </FadeUp>
+              <FadeUp delay={0.6} duration={1}>
+                <div className="mt-6 flex items-center justify-center gap-3">
+                  <Link href="/docs/" passHref>
+                    <Button
+                      variant="default"
+                      className="flex items-center justify-center gap-1"
+                    >
+                      Get started
+                      <ChevronRight size={16} />
+                    </Button>
+                  </Link>
+                  <Link
+                    href="https://github.com/extendui/extendui"
+                    passHref
+                    target="_blank"
+                    rel="noopener noreferrer"
                   >
-                    Get started
-                    <ChevronRight size={16} />
-                  </Button>
-                </Link>
-                <Link
-                  href="https://github.com/extendui/extendui"
-                  passHref
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <Button
-                    variant="outline"
-                    className="flex items-center justify-center gap-1"
-                  >
-                    <Star size={18} /> Star on GitHub
-                  </Button>
-                </Link>
-              </div>
-              <div className="mt-6 flex items-center justify-center gap-3">
-                <Icons.react className="h-8 w-8" />
-                <Icons.tailwind className="h-8 w-8" />
-                <Icons.radix className="h-8 w-8" />
-                <Icons.motion className="h-8 w-8" />
-              </div>
+                    <Button
+                      variant="outline"
+                      className="flex items-center justify-center gap-1"
+                    >
+                      <Star size={18} /> Star on GitHub
+                    </Button>
+                  </Link>
+                </div>
+                <div className="mt-6 flex items-center justify-center gap-3">
+                  <Icons.react className="h-8 w-8" />
+                  <Icons.tailwind className="h-8 w-8" />
+                  <Icons.radix className="h-8 w-8" />
+                  <Icons.motion className="h-8 w-8" />
+                </div>
+              </FadeUp>
               <div className="gradient pointer-events-none absolute inset-0 -z-10 block opacity-30 blur-3xl"></div>
             </div>
           </div>
@@ -63,21 +70,38 @@ export default function HomePage() {
       <section id="features" className="container mx-auto px-4 py-16">
         <h2 className="mb-8 text-3xl font-bold">Key Features</h2>
         <DynamicSteps>
-          <Step>Reusable Components</Step>
-          <p className="mt-2">
-            Build your UI faster with our pre-built, customizable components.
-          </p>
-
-          <Step>Performance Optimized</Step>
-          <p className="mt-2">
-            Enjoy improved application performance with our optimized component
-            library.
-          </p>
-
-          <Step>Easy Integration</Step>
-          <p className="mt-2">
-            Seamlessly integrate with your existing React and Next.js projects.
-          </p>
+          <Step>
+            <FadeUp delay={0.2} duration={0.8}>
+              Reusable Components
+            </FadeUp>
+          </Step>
+          <FadeUp delay={0.25} duration={0.8}>
+            <p className="mt-2">
+              Build your UI faster with our pre-built, customizable components.
+            </p>
+          </FadeUp>
+          <Step>
+            <FadeUp delay={0.4} duration={0.8}>
+              Performance Optimized
+            </FadeUp>
+          </Step>
+          <FadeUp delay={0.45} duration={0.8}>
+            <p className="mt-2">
+              Enjoy improved application performance with our optimized
+              component library.
+            </p>
+          </FadeUp>
+          <Step>
+            <FadeUp delay={0.6} duration={0.8}>
+              Easy Integration
+            </FadeUp>
+          </Step>
+          <FadeUp delay={0.65} duration={0.8}>
+            <p className="mt-2">
+              Seamlessly integrate with your existing React and Next.js
+              projects.
+            </p>
+          </FadeUp>
         </DynamicSteps>
       </section>
       <section
---
+++ src/components/fadeup.tsx
@@ -0,0 +1,37 @@
+'use client';
+import { useInView, motion } from 'framer-motion';
+import { ReactNode, useEffect, useRef, useState } from 'react';
+
+export default function FadeUp({
+  children,
+  delay = 0,
+  duration = 0.5,
+}: {
+  children: ReactNode;
+  delay?: number;
+  duration?: number;
+}) {
+  const ref = useRef(null);
+  const isInView = useInView(ref);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    if (isInView && !isVisible) {
+      setIsVisible(true);
+    }
+  }, [isInView, isVisible]);
+  return (
+    <motion.div
+      ref={ref}
+      initial={'hidden'}
+      variants={{
+        hidden: { opacity: 0, y: 15 },
+        visible: { opacity: 1, y: 0 },
+      }}
+      animate={isVisible ? 'visible' : 'hidden'}
+      transition={{ delay, duration, type: 'spring' }}
+    >
+      {children}
+    </motion.div>
+  );
+}
