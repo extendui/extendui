778f04b88dbfead210926c5c28008a11ef0a19f4
rynkovski <118986014+rynkovski@users.noreply.github.com>
2024-10-18 10:47:02 +0200

    feat: update features section

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,12 +1,12 @@
 import { Icons } from '@/components/icons/icons';
 import { Button } from '@/components/ui/button';
-import { ChevronRight, Star } from 'lucide-react';
+import { ChevronRight, Globe, Recycle, Star, Zap } from 'lucide-react';
 import Link from 'next/link';
-import Step from '@/components/step';
 import dynamic from 'next/dynamic';
 import ButtonExample from '@/showcase/components/Button/button';
 import ButtonSettingsEngine from '@/showcase/components/Button/settings-engine';
 import FadeUp from '@/components/fadeup';
+import { Card, CardContent } from '@/components/ui/card';
 
 const DynamicSteps = dynamic(() => import('@/components/steps'));
 
@@ -66,49 +66,57 @@ export default function HomePage() {
           </div>
         </div>
       </section>
-
-      <section id="features" className="container mx-auto px-4 py-16">
-        <h2 className="mb-8 text-3xl font-bold">Key Features</h2>
-        <DynamicSteps>
-          <Step>
-            <FadeUp delay={0.2} duration={0.8}>
-              Reusable Components
-            </FadeUp>
-          </Step>
-          <FadeUp delay={0.25} duration={0.8}>
-            <p className="mt-2">
-              Build your UI faster with our pre-built, customizable components.
-            </p>
+      <section
+        id="features"
+        className="container mx-auto px-4 py-16 md:py-24 lg:py-32"
+      >
+        <FadeUp delay={0.1} duration={0.8}>
+          <h2 className="mb-8 text-center text-3xl font-bold">Key Features</h2>
+        </FadeUp>
+        <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+          <FadeUp delay={0.2} duration={0.8}>
+            <Card className="border-none shadow-none">
+              <CardContent className="flex flex-col items-center space-y-2 p-6">
+                <Recycle className="h-12 w-12" />
+                <h3 className="text-xl font-bold"> Reusable Components</h3>
+                <p className="text-center text-gray-600">
+                  Build your UI faster with our pre-built, customizable
+                  components.
+                </p>
+              </CardContent>
+            </Card>
           </FadeUp>
-          <Step>
-            <FadeUp delay={0.4} duration={0.8}>
-              Performance Optimized
-            </FadeUp>
-          </Step>
-          <FadeUp delay={0.45} duration={0.8}>
-            <p className="mt-2">
-              Enjoy improved application performance with our optimized
-              component library.
-            </p>
+          <FadeUp delay={0.4} duration={0.8}>
+            <Card className="border-none shadow-none">
+              <CardContent className="flex flex-col items-center space-y-2 p-6">
+                <Zap className="h-12 w-12" />
+                <h3 className="text-xl font-bold"> Performance Optimized</h3>
+                <p className="text-center text-gray-600">
+                  Enjoy improved application performance with our optimized
+                  component library.
+                </p>
+              </CardContent>
+            </Card>
           </FadeUp>
-          <Step>
-            <FadeUp delay={0.6} duration={0.8}>
-              Easy Integration
-            </FadeUp>
-          </Step>
-          <FadeUp delay={0.65} duration={0.8}>
-            <p className="mt-2">
-              Seamlessly integrate with your existing React and Next.js
-              projects.
-            </p>
+          <FadeUp delay={0.6} duration={0.8}>
+            <Card className="border-none shadow-none">
+              <CardContent className="flex flex-col items-center space-y-2 p-6">
+                <Globe className="h-12 w-12" />
+                <h3 className="text-xl font-bold"> Easy Integration</h3>
+                <p className="text-center text-gray-600">
+                  Seamlessly integrate with your existing React and Next.js
+                  projects.
+                </p>
+              </CardContent>
+            </Card>
           </FadeUp>
-        </DynamicSteps>
+        </div>
       </section>
       <section
         id="components-showcase"
         className="container mx-auto px-4 py-16"
       >
-        <h2 className="mb-8 text-3xl font-bold">Components</h2>
+        <h2 className="mb-8 text-center text-3xl font-bold">Components</h2>
         <div className="relative w-full content-center rounded-lg py-4 shadow-[inset_0_0_2px_rgba(0,0,0,0.1)] shadow-slate-300">
           <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
             <div className="flex items-center justify-center p-4">
