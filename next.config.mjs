4616f4ae62724c9bcc59e6a2df8575d497600f36
Wicki Krzysztof <krzysiekwicki1@gmail.com>
2024-11-01 10:26:02 +0100

    test: paths

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,10 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    config.resolve.modules.push(process.cwd())
+    return config
+  },
 };
 
 const withContentlayer = createContentlayerPlugin({
