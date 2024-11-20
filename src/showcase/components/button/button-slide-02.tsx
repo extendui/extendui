0ae62e8e0799d1f8104a7dd5dd8b70dcf3d1fe41
Wicki Krzysztof <krzysiekwicki1@gmail.com>
2024-11-20 18:10:41 +0100

    feat: update button

--- src/showcase/components/button/button-slide-02.tsx
+++ src/showcase/components/button/button-slide-02.tsx
@@ -22,15 +22,15 @@ export function SlidingSpinningButton() {
         <>
             <motion.div
                 className={cn(
-                    `relative h-9 w-48 rounded-full overflow-hidden flex items-center justify-center text-2xl font-bold`,
-                    isOpen ? "bg-primary" : "bg-accent shadow-[inset_0px_0px_4px_rgba(0,0,0,0.8)]",
+                    `relative h-9 w-52 rounded-full overflow-hidden flex items-center justify-center text-2xl font-bold`,
+                    isOpen ? "bg-primary dark:bg-accent" : "bg-accent shadow-[inset_0px_0px_4px_rgba(0,0,0,0.8)]",
                 )}
             >
                 <div
                     className={cn(
                         "text-2xl font-bold",
                         isOpen
-                            ? "text-accent [text-shadow:1px_1px_1px_rgba(255,255,255,0.3),_-1px_-1px_1px_rgba(0,0,0,0.7)]"
+                            ? "text-accent dark:text-primary-foreground [text-shadow:1px_1px_1px_rgba(255,255,255,0.3),_-1px_-1px_1px_rgba(0,0,0,0.7)]"
                             : "text-primary-foreground [text-shadow:1px_1px_1px_rgba(255,255,255,0.3),_-1px_-1px_1px_rgba(0,0,0,0.5)]"
                     )}
                 >
@@ -39,7 +39,7 @@ export function SlidingSpinningButton() {
 
                 <motion.div
                     onClick={handleClick}
-                    animate={isSliding ? { x: 155, rotate: 360 } : { x: 0, rotate: 0 }}
+                    animate={isSliding ? { x: 172, rotate: 360 } : { x: 0, rotate: 0 }}
                     transition={{
                         duration: 1,
                         ease: "easeInOut"
@@ -47,11 +47,11 @@ export function SlidingSpinningButton() {
                     className="absolute left-0 h-full w-9 flex items-center justify-center rounded-full"
                 >
                     <Button
-                        className="rounded-full shadow-[0_0px_4px_1px_rgba(0,0,0,0.8)]"
+                        className="rounded-full shadow-[0_0px_4px_1px_rgba(0,0,0,0.8)] dark:bg-accent"
                         size="icon"
                         onClick={handleClick}
                     >
-                        {isOpen ? <LockKeyholeOpen className="size-5 text-accent" /> : <LockKeyhole className="size-5 text-accent" />}
+                        {isOpen ? <LockKeyholeOpen className="size-5 text-accent dark:text-primary-foreground" strokeWidth={3} /> : <LockKeyhole strokeWidth={3} className="size-5 text-accent dark:text-primary-foreground" />}
                     </Button>
                 </motion.div>
             </motion.div>
