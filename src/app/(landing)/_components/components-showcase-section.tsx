814e2824cc3444072f4782c907795a9a208ecc33
rynkovski <118986014+rynkovski@users.noreply.github.com>
2024-11-26 22:24:25 +0100

    feat: update components showcase

--- src/app/(landing)/_components/components-showcase-section.tsx
+++ src/app/(landing)/_components/components-showcase-section.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Autoplay from 'embla-carousel-autoplay';
+import { Upload } from 'lucide-react';
 
 import ComponentShowcaseCard from '@/components/component-showcase-card';
 import FadeUp from '@/components/fadeup';
@@ -22,46 +23,58 @@ const componentShowcases = [
   {
     href: '/docs/components/button',
     title: 'Button',
-    description: '23 variants',
+    description: '25+ variants',
     component: <ButtonExample />,
   },
   {
     href: '/docs/components/input',
     title: 'Input',
-    description: '16 variants',
+    description: '17+ variants',
     component: <InputExample />,
   },
   {
     href: '/docs/components/select',
     title: 'Select',
-    description: '5 variants',
+    description: '7+ variants',
     component: <SelectExample />,
   },
   {
     href: '/docs/components/command',
     title: 'Command',
-    description: '5 variants',
+    description: '3+ variants',
     component: <CommandExample />,
   },
   {
     href: '/docs/components/date-picker',
     title: 'Date picker',
-    description: '5 variants',
+    description: '5+ variants',
     component: <DatePickerExample />,
   },
 ];
 
 const blockShowcases = [
   {
-    href: '/docs/blocks/pricing-card',
+    href: '/docs/blocks/cards/pricing-card',
     title: 'Cards',
-    description: '3 variants',
+    description: '4 variants',
     component: (
       <Card className="w-[200px]">
-        <div className="space-y-3 p-6">
-          <div className="h-2 w-full rounded bg-gray-300"></div>
-          <div className="h-8 w-full rounded bg-gray-300"></div>
-          <div className="h-4 w-full rounded bg-gray-300"></div>
+        <div className="flex flex-col items-center justify-center space-y-3 p-6">
+          <div className="h-2 w-4/5 rounded-sm bg-gray-300"></div>
+          <div className="h-2 w-2/3 rounded-sm bg-gray-300"></div>
+          <div className="flex w-full items-center justify-around gap-2">
+            <span className="h-2 w-2 rounded-full bg-gray-300"></span>
+            <div className="h-1 w-full rounded-sm bg-gray-300"></div>
+          </div>{' '}
+          <div className="flex w-full items-center justify-around gap-2">
+            <span className="h-2 w-2 rounded-full bg-gray-300"></span>
+            <div className="h-1 w-full rounded-sm bg-gray-300"></div>
+          </div>{' '}
+          <div className="flex w-full items-center justify-around gap-2">
+            <span className="h-2 w-2 rounded-full bg-gray-300"></span>
+            <div className="h-1 w-full rounded-sm bg-gray-300"></div>
+          </div>
+          <div className="h-4 w-1/3 rounded-sm bg-gray-300"></div>
         </div>
       </Card>
     ),
@@ -70,13 +83,31 @@ const blockShowcases = [
     href: '/docs/blocks/file-upload',
     title: 'File Upload',
     description: '1 variant',
-    component: <span>File Upload</span>,
+    component: (
+      <Card className="w-[200px]">
+        <div className="space-y-3 p-6">
+          <div className="flex h-10 w-full items-center justify-center rounded border-2 border-dashed border-gray-300">
+            <Upload />
+          </div>
+        </div>
+      </Card>
+    ),
   },
   {
-    href: '/docs/blocks/sign-in',
-    title: 'Sign In',
-    description: '1 variant',
-    component: <span>Sign In</span>,
+    href: '/docs/blocks/forms/sign-in',
+    title: 'Forms',
+    description: '2 variants',
+    component: (
+      <Card className="w-[200px]">
+        <div className="flex flex-col items-center justify-center space-y-3 p-6">
+          <div className="h-2 w-1/3 rounded-sm bg-gray-300"></div>
+          <div className="h-3 w-1/2 rounded-sm bg-gray-300"></div>
+          <div className="h-3 w-1/2 rounded-sm bg-gray-300"></div>
+          <div className="h-3 w-1/2 rounded-sm bg-gray-300"></div>
+          <div className="h-3 w-1/3 rounded-sm bg-gray-300"></div>
+        </div>
+      </Card>
+    ),
   },
 ];
 
