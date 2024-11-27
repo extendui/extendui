1ae87209a76eb3fe638f4b2a737cd11cbc7eaf5b
Wicki Krzysztof <krzysiekwicki1@gmail.com>
2024-11-27 09:31:52 +0100

    feat: create placeholders

--- src/app/(landing)/_components/components-showcase-section.tsx
+++ src/app/(landing)/_components/components-showcase-section.tsx
@@ -1,11 +1,17 @@
 'use client';
 
 import Autoplay from 'embla-carousel-autoplay';
-import { Calendar, ChevronDown, ChevronsDown, ChevronsUpDown, Upload } from 'lucide-react';
 
 import ComponentShowcaseCard from '@/components/component-showcase-card';
 import FadeUp from '@/components/fadeup';
-import { Card } from '@/components/ui/card';
+import { ButtonPlaceholder } from '@/components/placeholders/button-placeholder';
+import { CardsPlaceholder } from '@/components/placeholders/cards-placeholder';
+import { CommandPlaceholder } from '@/components/placeholders/command-placeholder';
+import { DatePickerPlaceholder } from '@/components/placeholders/date-picker-placeholder';
+import { FileUploadPlaceholder } from '@/components/placeholders/file-upload-placeholder';
+import { FormsPlaceholder } from '@/components/placeholders/forms-placeholder';
+import { InputPlaceholder } from '@/components/placeholders/input-placeholder';
+import { SelectPlaceholder } from '@/components/placeholders/select-placeholder';
 import {
   Carousel,
   CarouselContent,
@@ -13,71 +19,37 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from '@/components/ui/carousel';
-import ButtonExample from '@/showcase/components/button/button';
-import { CommandExample } from '@/showcase/components/command/command';
-import DatePickerExample from '@/showcase/components/date-picker/date-picker';
-import { InputExample } from '@/showcase/components/input/input';
-import { SelectExample } from '@/showcase/components/select/select';
 
 const componentShowcases = [
   {
     href: '/docs/components/button',
     title: 'Button',
     description: '25+ variants',
-    component: (
-      <Card className="w-[120px] h-9 border-accent border-2 flex items-center justify-center">
-        <div className="w-16 h-4 rounded-sm bg-gray-200" />
-      </Card>
-    ),
+    component: <ButtonPlaceholder />,
   },
   {
     href: '/docs/components/input',
     title: 'Input',
     description: '17+ variants',
-    component: (
-      <Card className="w-[200px] h-9 border-accent border-2 flex items-center justify-start">
-        <div className="w-24 h-4 rounded-sm ml-2 bg-gray-200" />
-      </Card>
-    ),
+    component: <InputPlaceholder />,
   },
   {
     href: '/docs/components/select',
     title: 'Select',
     description: '7+ variants',
-    component: (
-      <Card className="w-[200px] h-9 border-accent border-2 flex items-center justify-between px-2">
-        <div className="w-24 h-4 rounded-sm bg-gray-200" />
-        <div>
-          <ChevronsUpDown className='text-accent' size={16} />
-        </div>
-      </Card>
-    ),
+    component: <SelectPlaceholder />,
   },
   {
     href: '/docs/components/command',
     title: 'Command',
     description: '3+ variants',
-    component: (
-      <Card className="w-[200px] h-9 border-accent border-2 flex items-center justify-between px-2">
-        <div className="w-24 h-4 rounded-sm bg-gray-200" />
-        <div>
-          <ChevronDown className='text-accent' size={16} />
-        </div>
-      </Card>
-    ),
+    component: <CommandPlaceholder />
   },
   {
     href: '/docs/components/date-picker',
     title: 'Date picker',
     description: '5+ variants',
-    component: (
-      <Card className="w-[200px] h-9 border-accent border-2 flex items-center justify-between px-2">
-        <div className="w-24 h-4 rounded-sm bg-gray-200" />
-        <div>
-          <Calendar className='text-accent' size={16} />
-        </div>
-      </Card>
-    ),
+    component: <DatePickerPlaceholder />
   },
 ];
 
@@ -86,64 +58,19 @@ const blockShowcases = [
     href: '/docs/blocks/cards/pricing-card',
     title: 'Cards',
     description: '4 variants',
-    component: (
-      <Card className="w-[180px] border-accent border-2">
-        <div className="flex flex-col justify-center  p-4">
-          <div className='space-y-2 items-start'>
-            <div className="h-2 w-2/5 rounded-sm mb-4 bg-gray-200" />
-            <div className="h-2 w-11/12 rounded-sm bg-gray-200" />
-            <div className="h-2 w-10/12 rounded-sm bg-gray-200" />
-            <div className="h-2 w-9/12 rounded-sm bg-gray-200" />
-          </div>
-          <div className="space-y-3 flex justify-end items-end">
-            <div className="h-3 w-3/12 rounded-xl mt-4 bg-accent" />
-          </div>
-        </div>
-      </Card>
-    ),
+    component: <CardsPlaceholder />
   },
   {
     href: '/docs/blocks/file-upload',
     title: 'File Upload',
     description: '1 variant',
-    component: (
-      <Card className="w-[180px] border-accent border-2">
-        <div className="space-y-3 p-6">
-          <div className="flex h-10 w-full items-center justify-center rounded border-2 border-dashed border-gray-200">
-            <Upload className='text-accent' />
-          </div>
-        </div>
-      </Card>
-    ),
+    component: <FileUploadPlaceholder />
   },
   {
     href: '/docs/blocks/forms/sign-in',
     title: 'Forms',
     description: '2 variants',
-    component: (
-      <Card className="w-[180px] border-accent border-2">
-        <div className="flex flex-col justify-center  p-4">
-          <div className='space-y-2 items-start'>
-            <div className="h-2 w-2/5 rounded-sm mb-4 bg-gray-200" />
-            <div className='grid grid-cols-2 gap-2'>
-              <div className="h-2 rounded-sm bg-gray-200" />
-              <div className="h-2 rounded-sm bg-gray-200" />
-            </div>
-            <div className='grid grid-cols-2 gap-2'>
-              <div className="h-2 rounded-sm bg-gray-200" />
-              <div className="h-2 rounded-sm bg-gray-200" />
-            </div>
-            <div className='grid grid-cols-2 gap-2'>
-              <div className="h-2 rounded-sm bg-gray-200" />
-              <div className="h-2 rounded-sm bg-gray-200" />
-            </div>
-          </div>
-          <div className="space-y-3 flex justify-end items-end">
-            <div className="h-3 w-3/12 rounded-xl mt-4 bg-accent" />
-          </div>
-        </div>
-      </Card>
-    ),
+    component: <FormsPlaceholder />
   },
 ];
 
---
+++ src/components/placeholders/button-placeholder.tsx
@@ -0,0 +1,9 @@
+import { Card } from "../ui/card"
+
+export const ButtonPlaceholder = () => {
+    return (
+        <Card className="w-[120px] h-9 border-accent border-2 flex items-center justify-center">
+            <div className="w-16 h-4 rounded-sm bg-gray-200" />
+        </Card>
+    )
+}
