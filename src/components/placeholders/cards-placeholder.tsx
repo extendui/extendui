4c38ab418462415f8eafcda1a781e85e5e6d0454
Wicki Krzysztof <krzysiekwicki1@gmail.com>
2024-11-27 09:44:43 +0100

    feat: update dark theme

--- src/components/placeholders/cards-placeholder.tsx
+++ src/components/placeholders/cards-placeholder.tsx
@@ -5,10 +5,10 @@ export const CardsPlaceholder = () => {
     <Card className="w-[180px] border-accent border-2">
       <div className="flex flex-col justify-center  p-4">
         <div className='space-y-2 items-start'>
-          <div className="h-2 w-2/5 rounded-sm mb-4 bg-gray-200" />
-          <div className="h-2 w-11/12 rounded-sm bg-gray-200" />
-          <div className="h-2 w-10/12 rounded-sm bg-gray-200" />
-          <div className="h-2 w-9/12 rounded-sm bg-gray-200" />
+          <div className="h-2 w-2/5 rounded-sm mb-4 bg-gray-200  dark:bg-gray-200/80" />
+          <div className="h-2 w-11/12 rounded-sm bg-gray-200 dark:bg-gray-200/80" />
+          <div className="h-2 w-10/12 rounded-sm bg-gray-200 dark:bg-gray-200/80" />
+          <div className="h-2 w-9/12 rounded-sm bg-gray-200 dark:bg-gray-200/80" />
         </div>
         <div className="space-y-3 flex justify-end items-end">
           <div className="h-3 w-3/12 rounded-xl mt-4 bg-accent" />
--- src/components/placeholders/forms-placeholder.tsx
+++ src/components/placeholders/forms-placeholder.tsx
@@ -5,18 +5,18 @@ export const FormsPlaceholder = () => {
     <Card className="w-[180px] border-accent border-2">
       <div className="flex flex-col justify-center  p-4">
         <div className='space-y-2 items-start'>
-          <div className="h-2 w-2/5 rounded-sm mb-4 bg-gray-200" />
+          <div className="h-2 w-2/5 rounded-sm mb-4 bg-gray-200 dark:bg-gray-200/80" />
           <div className='grid grid-cols-2 gap-2'>
-            <div className="h-2 rounded-sm bg-gray-200" />
-            <div className="h-2 rounded-sm bg-gray-200" />
+            <div className="h-2 rounded-sm bg-gray-200 dark:bg-gray-200/80" />
+            <div className="h-2 rounded-sm bg-gray-200 dark:bg-gray-200/80" />
           </div>
           <div className='grid grid-cols-2 gap-2'>
-            <div className="h-2 rounded-sm bg-gray-200" />
-            <div className="h-2 rounded-sm bg-gray-200" />
+            <div className="h-2 rounded-sm bg-gray-200 dark:bg-gray-200/80" />
+            <div className="h-2 rounded-sm bg-gray-200 dark:bg-gray-200/80" />
           </div>
           <div className='grid grid-cols-2 gap-2'>
-            <div className="h-2 rounded-sm bg-gray-200" />
-            <div className="h-2 rounded-sm bg-gray-200" />
+            <div className="h-2 rounded-sm bg-gray-200 dark:bg-gray-200/80" />
+            <div className="h-2 rounded-sm bg-gray-200 dark:bg-gray-200/80" />
           </div>
         </div>
         <div className="space-y-3 flex justify-end items-end">
